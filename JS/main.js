b1a39ba958e697a9652a3b19d26075d7defd8014
Erica Kermani <FarsiForeplay@Ericas-MBP-2.wireless.newschool.edu>
2016-08-16 14:09:07 -0400

    Website-Basic-Format

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+// var card [
+// 	["card1", "img/image.jpg", "info"],
+// 	["card2", "img", "info"],
+
+// ];
+// card[0][0] == 
+
+// for (var i = 0; i<cards.length; i++) {
+// 	card = cards[i]
+// };
