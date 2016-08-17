844a7b6a7c45bcb27410f5cf8fae56c399ac48cf
Erica Kermani <FarsiForeplay@Ericas-MBP-2.wireless.newschool.edu>
2016-08-17 11:53:32 -0400

    Adding JavaScript & CSS Animations

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,99 @@
-// var card [
-// 	["card1", "img/image.jpg", "info"],
-// 	["card2", "img", "info"],
+onload=alert("ERROR: Decipher Failed\nEncryption Detected.");
 
-// ];
-// card[0][0] == 
+//Cursor artwork created by Ervin Bolat from the Noun Project
+// function setCursorByID(laser,pointer) {
+//  var elem;
+//  if (document.getElementById && (elem=document.getElementById("laser")) ) {
+//   if (elem.style) elem.style.cursor=cursorStyle;
+//  }
+// }
 
-// for (var i = 0; i<cards.length; i++) {
-// 	card = cards[i]
-// };
+//Leftside info
+//text animation
+
+
+
+//Haftseen 1, Sabzeh
+//click, alert --> alert onload
+	
+//Haftseen 2, Seeb
+//click, image + description
+
+// onclick = function() {
+// 	document.getElementById("seeb").addEventListener("click", 
+// 	document.getElementById("english").style.display;
+// 	// this is function within
+
+// 	console.log("finished");
+	
+// });
+
+//Haftseen 3, sumac
+//hover, farsi 
+//click, language does not exist alert
+
+// onmouseover=document.getElementById('input').style.backgroundColor='Blue';">
+// <input id="input">
+// 	alert("ERROR: Language unknown.")
+
+
+//Haftseen 4, Sombol
+//click, scramble animation
+
+
+//Haftseen 5, seer
+//click, image + description
+
+//Haftseen 6, sehkeh (money)
+//hover, credit card
+//click, description
+
+
+//Haftseen 7, vinegar
+//hover, scramble animation
+// var scramble = document.getElementById("vinegar");
+
+// scramble.onclick=function(){
+// 	document.getElementById("");	
+// 	scramble.classList.toggle("");
+// }
+
+
+//Haftseen 8, kehtab
+//click, binary code/english popup
+
+var header = document.getElementById("header");
+var half_kehbab = document.getElementById("kehbab");
+
+var actions = function() {
+	header.classList.add('is-hidden');
+}
+
+var actions_reverse = function() {
+	header.classList.remove('is-hidden');
+}
+
+
+
+header.addEventListener("click", actions, false);
+header.addEventListener("mouseover", actions, false);
+header.addEventListener("mouseout", actions_reverse, false);
+
+// /;/ var hafiz = document.getElementById("kehtab");
+
+// // hafiz.addEventListener("mouseover")
+
+// //applying a CSS animation
+
+// hafiz.ondblclick = function(){
+// 	boxfive.classList.toggle("do-spin");
+// 	boxfive.classList.add("kehtab");
+// }
+
+//Haftseen 9, ayneh
+//click, image + description
+
+
+
+
+
